624777a96f9e254c343f06a0de55b08c41d40c07
SabaMuladze <muladze66@bk.ru>
2024-01-29 22:53:09 +0400

    create project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap");
+
+* {
+  font-family: "Libre Baskerville", serif;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <div>galleria</div>
+  </header>
+</template>
+
+<script></script>
+
+<style scoped></style>
