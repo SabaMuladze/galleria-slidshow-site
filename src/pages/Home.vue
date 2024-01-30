b116915b1338c6b06ed08f082d9268061de3453b
SabaMuladze <muladze66@bk.ru>
2024-01-30 17:12:43 +0400

    style: add titles

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,21 +3,37 @@
     <div class="card-col">
       <div class="card" v-for="card in data[0]" :key="card.name">
         <img :src="card.images.thumbnail" alt="wr" />
+        <div class="title">
+          <h2>{{ card.name }}</h2>
+          <p>{{ card.artist.name }}</p>
+        </div>
       </div>
     </div>
     <div class="card-col">
       <div class="card" v-for="card in data[1]" :key="card.name">
         <img :src="card.images.thumbnail" alt="wr" />
+        <div class="title">
+          <h2>{{ card.name }}</h2>
+          <p>{{ card.artist.name }}</p>
+        </div>
       </div>
     </div>
     <div class="card-col">
       <div class="card" v-for="card in data[2]" :key="card.name">
         <img :src="card.images.thumbnail" alt="wr" />
+        <div class="title">
+          <h2>{{ card.name }}</h2>
+          <p>{{ card.artist.name }}</p>
+        </div>
       </div>
     </div>
     <div class="card-col">
       <div class="card" v-for="card in data[3]" :key="card.name">
         <img :src="card.images.thumbnail" alt="wr" />
+        <div class="title">
+          <h2>{{ card.name }}</h2>
+          <p>{{ card.artist.name }}</p>
+        </div>
       </div>
     </div>
   </section>
@@ -57,6 +73,35 @@ export default {
   height: 100%;
   width: 100%;
 }
+
+.card {
+  position: relative;
+}
+.title {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  bottom: 33px;
+  left: 32px;
+}
+h2 {
+  position: absolute;
+  color: white;
+  bottom: 20px;
+}
+p {
+  position: absolute;
+  bottom: 0;
+  opacity: 0.7528;
+  color: white;
+}
+img {
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.84) 100%
+  );
+}
 @media (min-width: 688px) {
   .cards {
     flex-direction: row;
@@ -80,6 +125,9 @@ export default {
   .cards {
     padding-left: 50px;
     padding-right: 50px;
+  }
+  .card {
+    cursor: pointer;
   }
 }
 
